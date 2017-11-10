eb6e8b94d23d6601e66a18c9dd1304742d417711
J David Smith <emallson@atlanis.net>
2017-11-10 17:18:19 -0500

    Added parsing of amounts into values for +0/10/20

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate logger;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate separator;
 
 extern crate askama;
 #[macro_use]
@@ -21,6 +22,7 @@ use std::path::Path;
 use askama::Template;
 use iron::headers::ContentType;
 use iron::modifiers::Header;
+use separator::Separatable;
 
 #[derive(Template)]
 #[template(path = "main.html")]
@@ -40,7 +42,53 @@ struct MobTemplate<'a, 'b> {
 struct Ability<'a> {
     id: usize,
     name: &'a str,
-    description: &'a str,
+    base_description: &'a str,
+    /// Contains the mythic +0 amounts of damage/healing/whatever done by the ability
+    amounts: &'a [usize],
+    description: String,
+}
+
+impl<'a> Ability<'a> {
+    fn new(id: usize, name: &'a str, base_description: &'a str, amounts: &'a [usize]) -> Self {
+        let mut ab = Ability {
+            id, name, base_description, amounts, description: "".to_owned()
+        };
+        ab.description = ab.describe();
+        ab
+    }
+
+    fn describe(&self) -> String {
+        const LEVELS: &[usize] = &[1, 10, 20];
+        let mut owned = self.base_description.to_owned();
+        for &amount in self.amounts {
+            let (low, mid, high) = scale(amount, LEVELS);
+            let tpl = AmountTemplate {
+                    amount_base: low,
+                    amount_mid: mid,
+                    amount_high: high,
+                }
+                .render()
+                .unwrap();
+
+            owned = owned.replace("%d", &tpl);
+        }
+
+        owned
+    }
+}
+
+#[derive(Template)]
+#[template(path = "amount3.html")]
+struct AmountTemplate {
+    amount_base: usize,
+    amount_mid: usize,
+    amount_high: usize,
+}
+
+fn scale(amount: usize, levels: &[usize]) -> (usize, usize, usize) {
+    assert_eq!(levels.len(), 3);
+    let s = |level| (amount as f64 * 1.1f64.powi(level as i32 - 1)).ceil() as usize;
+    (s(levels[0]), s(levels[1]), s(levels[2]))
 }
 
 fn mob(_: &mut Request) -> IronResult<Response> {
@@ -49,19 +97,20 @@ fn mob(_: &mut Request) -> IronResult<Response> {
                        MobTemplate {
                            name: "Tank Butcherer",
                            percent: 2.0,
-                           abilities: &[Ability {
-                                            id: 0,
-                                            name: "Shadow Slash",
-                                            description: "Deals YUUUGE Shadow damage to all \
-                                                          players in a frontal cone. Will not \
+                           abilities: &[Ability::new(0,
+                                                  "Shadow Slash",
+                                                  "Deals YUUUGE Shadow damage to all \
+                                                  players in a frontal cone. Will not \
                                                           turn while casting.",
-                                        },
-                                        Ability {
-                                            id: 0,
-                                            name: "Penetrating Shot",
-                                            description: "Kills the nearest Mage immediately \
-                                                          after they hit Combustion.",
-                                        }],
+                                                          &[],
+                                                          ),
+                                                          Ability::new(0,
+                                                                    "Penetrating Shot",
+                                                                    "Deals %d physical damage in a line 45 \
+                                                                    yards in front of the caster. Ignores \
+                                                          armor.",
+                                                          &[1_600_000],
+                                                          )],
                            teeming_percent: 1.2,
                            _parent: MainTemplate {},
                        })))
